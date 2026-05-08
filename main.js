01ea46527214082b883426566b15c15ea156fcda
szuryuu <ilhamdzaky2007@gmail.com>
2026-05-08 21:09:46 +0700

    feat(info): additional card

--- main.js
+++ main.js
@@ -58,6 +58,20 @@ class ProfileHero extends HTMLElement {
 class LinkGrid extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
+      <a href="https://ilhamdzaky.my.id" target="_blank" rel="noopener" class="link-card">
+        <div class="link-icon"><i data-lucide="globe"></i></div>
+        <div class="link-content">
+          <span class="link-title">Personal Website</span>
+          <span class="link-url">ilhamdzaky.my.id</span>
+        </div>
+      </a>
+      <a href="https://ilhamdzaky.my.id/cv-2026.pdf" target="_blank" rel="noopener" class="link-card">
+        <div class="link-icon"><i data-lucide="file-text"></i></div>
+        <div class="link-content">
+          <span class="link-title">Curriculum Vitae</span>
+          <span class="link-url">CV 2026 (PDF)</span>
+        </div>
+      </a>
       <a href="/blog" class="link-card">
         <div class="link-icon"><i data-lucide="book-open"></i></div>
         <div class="link-content">
